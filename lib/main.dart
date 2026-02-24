5ec9984a3f5896e01b3dea1d4dd236c09f2398ca
oleder1337 <oleder98@gmx.de>
2026-02-24 13:55:12 +0100

    implemented ios capabilities

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
-import 'src/presentation/home/home_page.dart';
 
 void main() {
-  runApp(const StreamlioApp());
+  runApp(const MyApp());
 }
 
-class StreamlioApp extends StatelessWidget {
-  const StreamlioApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Streamlio',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(),
+    return const MaterialApp(
+      home: HelloWorld(),
+    );
+  }
+}
+
+class HelloWorld extends StatelessWidget {
+  const HelloWorld({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
     );
   }
 }
